eb7a44a6a82b9cd51c8aa49271ff2c5f4ea13c4d
KingJinxed <a31345@halcones.ual.mx>
2025-11-05 12:58:06 -0600

    a

--- JS/Practica6.js
+++ JS/Practica6.js
@@ -1,15 +1,17 @@
 function secuenciaCompleta()
 {
-    let numero = 50;
+    let inicio = 2;
+    let suma = 4;
     let contador = 1;
     let resultado = "";
 
     while (contador <= 10)
     {
-        resultado += numero  + (contador < 10 ? ", " : "");
-        numero *= 5;
+        resultado += inicio  + (contador < 10 ? ", " : "");
+        inicio += suma;
+        suma += 2;
         contador++;
         alert("La secuencia es: " + resultado);
     }
-            alert("Ya no hay mas");
 }
+//2 6 12 20
---
+++ JS/Practica7.js
@@ -0,0 +1,5 @@
+function numeroCaracteres()
+{
+    let nombreC = prompt("Dame tu nombre completo")
+    alert("Numero de caracteres" + nombreC.length)
+}
