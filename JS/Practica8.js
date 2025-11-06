91dc412d804be74d321eabf39fb8a30a45fa1f0b
KingJinxed <a31345@halcones.ual.mx>
2025-11-06 01:44:33 -0600

    a

--- JS/Practica8.js
+++ JS/Practica8.js
@@ -1,4 +1,16 @@
 function validarCamposNulos()
 {
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = document.getElementById("telefono").value;
     
+    if (nombre === "" || apellidos === "" || correo === "" || telefono === "") 
+        {
+            alert("Por favor llene todos los campos.");
+        } 
+        else 
+            {
+                alert("Formulario enviado correctamente");
+            }
 }
