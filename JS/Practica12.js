46bef5586d5ade347669919882bda8bd6a042591
KingJinxed <a31345@halcones.ual.mx>
2025-11-10 12:47:08 -0600

    a

--- JS/Practica12.js
+++ JS/Practica12.js
@@ -1,8 +1,9 @@
 function listaDeEspera()
 {
     let personasEspera = [Alfonso, Wildesito, Leonardo, Tamayo, Erik, Luis, Obed, victoria, Fernando,Lili, Mich,Oscar,Liam,Hugo,Marco];
-
     let nom = prompt("¿Cual es tu nombre?")
+
+    
     for(let i = 0; ; i++)
     {
         if(arregloNum [i] % 2 == 0)
