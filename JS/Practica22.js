da61d68ca0f094a6f119c44b92b8fb6e5a41650d
KingJinxed <a31345@halcones.ual.mx>
2025-11-20 13:07:49 -0600

    AA

---
+++ JS/Practica22.js
@@ -0,0 +1,16 @@
+function listaDinamica()
+{
+    var nueDiv = document.createElement("Div")
+    var nueConte = document.textContent("")
+    nueDiv.appendChild(nueConte)
+
+}
+
+function agregarNuevoItemColor()
+{
+    var nueBoton = document.createElement("button")
+    var nueTexto = document.createElement("p")
+    const nombre = document.getElementsByName("nombre");
+    //la funcion debe recicibir como parametro el nomnre de uin color, ingresado por el usuario a traves del campo de texo creado
+    
+}
---
+++ JS/Practica23.js
@@ -0,0 +1,20 @@
+//["Mexico", "Colombia", "Brasil", "Argentina", "Guatemala" "Chile"]
+let paises = ["Mexico", "Colombia", "Brasil", "Argentina", "Guatemala", "Chile"];
+function mostrarPaises()
+{
+    let mensaje = "<ol>";
+    for (let i = 0; i < paises.length; i++) 
+    {
+        if (paises[i].length > 7) 
+        {
+            mensaje += `<li style="color: red;">${paises[i]}</li>`;
+        } 
+        else 
+        {
+            mensaje += `<li>${paises[i]}</li>`;
+        }
+}
+//Realizar una  funcion que muestre y cree una lista ordenada en html de los paises
+    mensaje += "</ol>";
+    document.getElementById("listaPaises").innerHTML = mensaje;
+}
