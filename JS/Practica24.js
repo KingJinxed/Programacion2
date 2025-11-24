f320079ba012ad21723128a805ce1757addc1cc7
KingJinxed <a31345@halcones.ual.mx>
2025-11-24 13:26:23 -0600

    aaa

--- JS/Practica24.js
+++ JS/Practica24.js
@@ -1,16 +1,18 @@
-function divDinamicos()
+function cambiarimagen()
 {
     let imagenes = ["..//IMG/MONKY1.jpg", "..//IMG/MONKY2.jpg", "..//IMG/MONKY3.jpg" ,"..//IMG/JINXYYYYY.jpg"];
     let contador = 0;
-    for (let i = 0; i < imagenes.length; i++)
-    {
+    let i = 0;
         contador++;
         if (contador === 3)
         {
             contador = 0;
-            break;
+            imgElement.setAttribute("src", imagenes[contador]);
         }
-    }
+        else
+        {
+            imgElement.setAttribute("src", imagenes[contador]);
+        }
     let imgElement = document.getElementById("imagen");
-    imgElement.src = imagenes[contador];
-}
+    imgElement.src = imagenes[contador]; 
+    }
