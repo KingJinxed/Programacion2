d6e6ee653f9a9dc16df5915d735800b30beaa711
KingJinxed <a31345@halcones.ual.mx>
2025-11-25 09:23:28 -0600

    a

--- JS/Practica24.js
+++ JS/Practica24.js
@@ -2,17 +2,15 @@ function cambiarimagen()
 {
     let imagenes = ["..//IMG/MONKY1.jpg", "..//IMG/MONKY2.jpg", "..//IMG/MONKY3.jpg" ,"..//IMG/JINXYYYYY.jpg"];
     let contador = 0;
-    let i = 0;
-        contador++;
-        if (contador === 3)
-        {
-            contador = 0;
-            imgElement.setAttribute("src", imagenes[contador]);
-        }
-        else
-        {
-            imgElement.setAttribute("src", imagenes[contador]);
-        }
+
     let imgElement = document.getElementById("imagen");
-    imgElement.src = imagenes[contador]; 
+
+    imgElement.src = imagenes[contador];
+    contador++;
+
+    if (contador >= imagenes.length) 
+    {
+        contador = 0;
     }
+
+}
